6c4dbe89fb929c15cecf98d21039f4f1180d059d
Luiza Oliveira <151765513+luedmais@users.noreply.github.com>
2025-02-13 23:09:12 -0300

    Infantaria.hpp

--- src/Infantaria.hpp
+++ src/Infantaria.hpp
@@ -1,16 +1,19 @@
 #ifndef INFANTARIA_HPP
 #define INFANTARIA_HPP
+// Garantia de inclusão única do arquivo de cabeçalho
 
-#include "Unidade.hpp"
+#include "Unidade.hpp"  // Inclui a classe base Unidade
 
+// Declaração da classe Infantaria, que herda de Unidade
 class Infantaria : public Unidade {
 private:
-    int forca;
-    int velocidade;
+    int forca;  // Atributo que representa a força da infantaria
+    int velocidade;  // Atributo que representa a velocidade da infantaria
 
 public:
-    Infantaria();  // Construtor
-    double getPoderAtaque() override;
+    Infantaria();  // Construtor da classe Infantaria
+    double getPoderAtaque() override;  // Sobrescrita do método para calcular o poder de ataque da infantaria
 };
 
 #endif
+// Fim da garantia de inclusão única do arquivo de cabeçalho
